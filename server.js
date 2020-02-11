bd7bc5d9a489ba88c5d55a5d7a8ffd39afb82f61
MaxymSemenykhin <msem@isddesign.com>
2020-02-11 17:43:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) { ////create a server object:
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
   
-}).listen(8080); //the server object listens on port 8080
+}).listen(process.env.NODE_PORT); //the server object listens on port 8080
