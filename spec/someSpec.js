2626b61dfa5520a6e59f38671ecc25736b7381a5
vagrant <vagrant@ilm-workstation.local>
2020-02-12 09:52:54 +0200

    Add jasmine tests

---
+++ spec/someSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe("alignment -> ", function() {
+
+  it("Basic word wrap ", function () {
+    expect(true).toEqual(true);
+  });
+
+  it("Basic word parse ", function () {
+    expect(true).toEqual(true);
+
+  });
+
+});
+
